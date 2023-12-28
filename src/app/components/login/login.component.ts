8c54e2e903a677432de0c0690e4467f7c1fc86c1
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-12-28 10:05:05 +0100

    login with auth interceptor

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(private authservice: AuthService, private router: Router) { }
+  // hier Service verknüpfen
+
+  async login() {
+    try{
+      let resp = await this.authservice.loginWithUserAndPassword(this.username, this.password);
+      console.log(resp);
+      localStorage.setItem('token', resp['token'])
+      this.router.navigateByUrl('/todos');
+    } catch(e) {
+      console.error(e);
+    }
+  }
 
 }
