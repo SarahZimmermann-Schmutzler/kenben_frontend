d0094b059fe6aee092f5c71fd94e618dde165a82
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-01-24 12:45:01 +0100

    get value of checkboxes

--- src/app/components/add-ticket/add-ticket.component.ts
+++ src/app/components/add-ticket/add-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-ticket',
@@ -7,8 +7,47 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class AddTicketComponent {
   @Output() addTask = new EventEmitter();
-  title = '';
-  description = '';
+  new_title = '';
+  new_description = '';
+  checkbox_low = false;
+  checkbox_middle = false;
+  checkbox_high = false;
+  prio_low = '';
+  prio_middle = '';
+  prio_high = '';
+  isDisabled = false;
+
+ ngOnInit() {
+  
+ }
+
+  createTicket() {
+    this.getPrioValue();
+
+    console.log(this.new_title);
+    console.log(this.new_description);
+    console.log(this.prio_low);
+    console.log(this.prio_middle);
+    console.log(this.prio_high);
+  }
+
+  disableCheckbox() {
+    this.isDisabled = true;
+  }
+
+  getPrioValue() {
+    if(this.checkbox_low == true) {
+      this.prio_low = 'low';
+    }
+
+    if(this.checkbox_middle == true) {
+      this.prio_middle = 'middle';
+    }
+
+    if(this.checkbox_high == true) {
+      this.prio_high = 'high';
+    }
+  }
 
   closeAddTicket() {
     this.addTask.emit(false);
