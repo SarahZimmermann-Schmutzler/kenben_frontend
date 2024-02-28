d406317ba30b1e927a87a6aa1ca14ed47d8ac28b
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-02-28 14:50:06 +0100

    environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
-    // baseURL: 'http://localhost:8000',
     baseURL:'https://sarahzimmermann.pythonanywhere.com',
 };
 
-// Entwicklungsmodus (mein Server)
+// Veröffentlichungsmodus
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
     baseURL: 'http://localhost:8000',
-    // baseURL:'https://sarahzimmermann.pythonanywhere.com',
 };
 
-// Veröffentlichung
+// Entwicklungsmodus (mein Server)
