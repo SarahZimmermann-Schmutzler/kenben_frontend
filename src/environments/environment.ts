d92ad35c6555f44f07edf4243df21332e78dae3d
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2024-03-30 14:46:10 +0100

    new basURL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseURL:'https://kenben.backend.s-zimmermann-schmutzler.de',
+    baseURL:'https://backend.s-zimmermann-schmutzler.de',
 };
 
 // Veröffentlichungsmodus 
